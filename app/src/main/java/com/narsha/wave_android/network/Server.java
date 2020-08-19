443418e8fbda27d41e425f0679ae9e4fcb0c5964
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-08-19 17:18:21 +0900

    dsadasdsadsa
    dsada

--- app/src/main/java/com/narsha/wave_android/network/Server.java
+++ app/src/main/java/com/narsha/wave_android/network/Server.java
@@ -1,4 +1,26 @@
 package com.narsha.wave_android.network;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class Server {
+    private static Server instance;
+    private ServerAPI api;
+    private Server() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        api = retrofit.create(ServerAPI.class);
+    }
+
+    public static Server getInstance() {
+        if (instance == null) instance = new Server();
+        return instance;
+    }
+
+    public ServerAPI getApi() {
+        return api;
+    }
 }
+
--- app/src/main/java/com/narsha/wave_android/network/ServerAPI.java
+++ app/src/main/java/com/narsha/wave_android/network/ServerAPI.java
@@ -1,4 +1,12 @@
 package com.narsha.wave_android.network;
 
+import com.narsha.wave_android.data.request.signup.SignUp;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 public interface ServerAPI {
+    @GET("users/{id}")
+    Call<SignUp> user(@Path("userId") String userId, @Path("password") String password);
 }
