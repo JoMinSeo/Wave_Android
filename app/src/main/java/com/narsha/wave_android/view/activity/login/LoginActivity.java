bd950d9705c4e7fa59c7c9aac13ba84dad58752f
Jominseo <minseocho0309@gmail.com>
2020-08-19 17:00:07 +0900

    dfdfdfd

--- app/src/main/java/com/narsha/wave_android/view/activity/login/LoginActivity.java
+++ app/src/main/java/com/narsha/wave_android/view/activity/login/LoginActivity.java
@@ -3,14 +3,21 @@ package com.narsha.wave_android.view.activity.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.narsha.wave_android.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText idEt, passEt;
+    Button checkButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+
 }
