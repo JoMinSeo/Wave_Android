a26e98eb93ae6edfc4fe00ea4ac980382b623b29
Jominseo <minseocho0309@gmail.com>
2020-08-19 16:36:11 +0900

    commit

---
+++ app/src/main/java/com/narsha/wave_android/view/activity/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.narsha.wave_android.view.activity.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.narsha.wave_android.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
