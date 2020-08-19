4b8f7fe6aed7146c84700e6732742f5ae1f61a85
DGSW_양동현 <66651059+DGSWDongHyun@users.noreply.github.com>
2020-08-19 17:44:44 +0900

    full screen & completed design.

--- app/src/main/java/com/narsha/wave_android/view/activity/main/MainActivity.java
+++ app/src/main/java/com/narsha/wave_android/view/activity/main/MainActivity.java
@@ -3,18 +3,24 @@ package com.narsha.wave_android.view.activity.main;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.narsha.wave_android.R;
+import com.narsha.wave_android.view.activity.login.LoginActivity;
 import com.narsha.wave_android.viewmodel.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
-
+    Boolean isLogined = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if(!isLogined){
+            finish();
+            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+        }
         MainViewModel mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
     }
 }
