6db6611df3e9a1f57ec77c95381aca5c10d7c9ce
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-08-19 18:02:23 +0900

    commit nfsadfiujsjhnidsoj

--- app/src/main/java/com/narsha/wave_android/network/ServerAPI.java
+++ app/src/main/java/com/narsha/wave_android/network/ServerAPI.java
@@ -1,7 +1,6 @@
 package com.narsha.wave_android.network;
 
-import com.narsha.wave_android.data.request.login.Login;
-import com.narsha.wave_android.data.request.signup.SignUp;
+import com.narsha.wave_android.data.User;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -9,8 +8,5 @@ import retrofit2.http.POST;
 
 public interface ServerAPI {
     @POST("login")
-    Call<Login> user(@Body Login login);
-
-    @POST("signUp")
-    Call<SignUp> user(@Body SignUp signUp);
+    Call<User> user(@Body User user);
 }
