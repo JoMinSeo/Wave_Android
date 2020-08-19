569f377fb070419561a5003609c786823dd6e4fa
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-08-19 17:33:55 +0900

    dsad
    dsad

---
+++ app/src/main/java/com/narsha/wave_android/data/request/login/Login.kt
@@ -0,0 +1,3 @@
+package com.narsha.wave_android.data.request.login
+
+data class Login(var userId : String? = null, var password : String?= null)
--- app/src/main/java/com/narsha/wave_android/network/ServerAPI.java
+++ app/src/main/java/com/narsha/wave_android/network/ServerAPI.java
@@ -1,12 +1,16 @@
 package com.narsha.wave_android.network;
 
+import com.narsha.wave_android.data.request.login.Login;
 import com.narsha.wave_android.data.request.signup.SignUp;
 
 import retrofit2.Call;
-import retrofit2.http.GET;
-import retrofit2.http.Path;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
 
 public interface ServerAPI {
-    @GET("users/{id}")
-    Call<SignUp> user(@Path("userId") String userId, @Path("password") String password);
+    @POST("login")
+    Call<Login> user(@Body Login login);
+
+    @POST("signUp")
+    Call<SignUp> user(@Body SignUp signUp);
 }
