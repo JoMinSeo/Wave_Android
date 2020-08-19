95d1c231808ea1557b7ca5124183f8d127d79f78
kimjw2003 <kimjw111003@gmail.com>
2020-08-19 18:01:25 +0900

    [COMMIT]

--- app/src/main/java/com/narsha/wave_android/viewmodel/MainViewModel.java
+++ app/src/main/java/com/narsha/wave_android/viewmodel/MainViewModel.java
@@ -1,6 +1,11 @@
 package com.narsha.wave_android.viewmodel;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.narsha.wave_android.data.User;
+
 public class MainViewModel extends ViewModel {
+    public MutableLiveData<User> user = new MutableLiveData<>();
+
 }
