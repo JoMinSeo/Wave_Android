6ebc1ff9bfdc2e1b827f80d8b80b0976b990b29b
DGSW_양동현 <66651059+DGSWDongHyun@users.noreply.github.com>
2020-08-26 18:09:15 +0900

    [ ADD ] Fragment

---
+++ app/src/main/java/com/narsha/wave_android/view/fragment/home/HomeFragment.java
@@ -0,0 +1,40 @@
+package com.narsha.wave_android.view.fragment.home;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.narsha.wave_android.R;
+
+public class HomeFragment extends Fragment {
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        return root;
+    }
+}
