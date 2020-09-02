f253a0558ae7be678386be74774290ed7acd29d4
DGSW_양동현 <66651059+DGSWDongHyun@users.noreply.github.com>
2020-09-02 16:57:02 +0900

    commit

---
+++ app/src/main/java/com/narsha/wave_android/data/slider/SliderItem.kt
@@ -0,0 +1,3 @@
+package com.narsha.wave_android.data.slider
+
+data class SliderItem(var ImageUrl : Int?= null, var Description : String ?= null)
--- app/src/main/java/com/narsha/wave_android/view/fragment/main/MainFragment.java
+++ app/src/main/java/com/narsha/wave_android/view/fragment/main/MainFragment.java
@@ -1,5 +1,6 @@
 package com.narsha.wave_android.view.fragment.main;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,10 +12,15 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.narsha.wave_android.R;
+import com.narsha.wave_android.data.slider.SliderItem;
+import com.narsha.wave_android.view.adapter.imageslider.MainImageSlider;
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
 
 public class MainFragment extends Fragment {
-
-
+    SliderView sliderView;
+    private MainImageSlider adapter;
     public MainFragment() {
         // Required empty public constructor
     }
@@ -34,7 +40,28 @@ public class MainFragment extends Fragment {
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
+        sliderView = root.findViewById(R.id.imageSlider);
+        adapter = new MainImageSlider(getContext());
+
+        addNewItem();
+        sliderView.setSliderAdapter(adapter);
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM); //set indicator animation by using SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
+        sliderView.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
+        sliderView.setAutoCycleDirection(SliderView.AUTO_CYCLE_DIRECTION_BACK_AND_FORTH);
+        sliderView.setIndicatorSelectedColor(Color.WHITE);
+        sliderView.setIndicatorUnselectedColor(Color.GRAY);
+        sliderView.setScrollTimeInSec(3);
+        sliderView.setAutoCycle(true);
+        sliderView.startAutoCycle();
 
         return root;
+    }
+    public void addNewItem() {
+        for(int i = 0 ; i < 10; i ++) {
+            SliderItem sliderItem = new SliderItem();
+            sliderItem.setDescription("Slider Item Added Manually "+i);
+            sliderItem.setImageUrl(R.color.colorAccent);
+            adapter.addItem(sliderItem);
+        }
     }
 }
