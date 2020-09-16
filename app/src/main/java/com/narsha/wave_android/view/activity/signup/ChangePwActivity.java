b43f7e5bebae00f17480876bb9592111d7ded0f9
kimjw2003 <kimjw111003@gmail.com>
2020-09-16 20:02:03 +0900

    [ADD] ChangePwActivity

---
+++ app/src/main/java/com/narsha/wave_android/view/activity/signup/ChangePwActivity.java
@@ -0,0 +1,16 @@
+package com.narsha.wave_android.view.activity.signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.narsha.wave_android.R;
+
+public class ChangePwActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_pw);
+    }
+}
