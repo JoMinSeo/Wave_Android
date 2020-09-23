38ebd1d2ae4fa869b865c75f9b166f2892419398
GyuSeok Nam <52943549+nks0614@users.noreply.github.com>
2020-09-23 17:50:15 +0900

    [ADD] Song Activity

---
+++ app/src/main/java/com/narsha/wave_android/view/activity/song/SongActivity.kt
@@ -0,0 +1,14 @@
+package com.narsha.wave_android.view.activity.song
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.narsha.wave_android.R
+
+class SongActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_song)
+
+
+    }
+}
