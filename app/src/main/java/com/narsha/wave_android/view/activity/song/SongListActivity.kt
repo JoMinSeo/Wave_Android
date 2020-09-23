d912d627bbf098aa710e044413dfc0539c2bf3e6
kimjw2003 <kimjw111003@gmail.com>
2020-09-23 19:27:35 +0900

    [ADD] RecyclerViewAdapter

---
+++ app/src/main/java/com/narsha/wave_android/view/activity/song/SongListActivity.kt
@@ -0,0 +1,12 @@
+package com.narsha.wave_android.view.activity.song
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.narsha.wave_android.R
+
+class SongListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_song_list)
+    }
+}
