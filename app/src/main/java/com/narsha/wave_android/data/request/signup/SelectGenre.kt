c2813862af09e62b832da4f59b007533e4421e84
kimjw2003 <kimjw111003@gmail.com>
2020-09-23 17:11:01 +0900

    [CLEAN] DataList

--- app/src/main/java/com/narsha/wave_android/data/request/signup/SelectGenre.kt
+++ app/src/main/java/com/narsha/wave_android/data/request/signup/SelectGenre.kt
@@ -3,5 +3,6 @@ package com.narsha.wave_android.data.request.signup
 import com.narsha.wave_android.data.request.genre.Genre
 
 data class SelectGenre(
-        var maingenreid: Int? = null, var name : String?=null
+        var maingenreid: Int? = null,
+        var name : String?=null
 )
