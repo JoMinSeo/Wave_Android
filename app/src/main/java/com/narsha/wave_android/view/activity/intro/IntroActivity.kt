5b62e3581477a8786bf377d184e40b48b3a7045d
GyuSeok Nam <52943549+nks0614@users.noreply.github.com>
2020-10-07 17:24:35 +0900

    [UPDATE] Intro

--- app/src/main/java/com/narsha/wave_android/view/activity/intro/IntroActivity.kt
+++ app/src/main/java/com/narsha/wave_android/view/activity/intro/IntroActivity.kt
@@ -9,15 +9,11 @@ import com.narsha.wave_android.view.activity.login.LoginActivity
 
 class IntroActivity : AppCompatActivity() {
 
-    private val INTRO_TIME_OUT:Long = 1300
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
 
-        Handler().postDelayed({
-                startActivity(Intent(this, LoginActivity::class.java))
-                finish()
-        },INTRO_TIME_OUT)
+        startActivity(Intent(this, LoginActivity::class.java))
+        finish()
     }
 }
