42d9fe43fa31a1ca01d45f7fb2a7b2dec2862c21
DGSW_양동현 <66651059+DGSWDongHyun@users.noreply.github.com>
2020-10-21 19:15:16 +0900

    [MODIFY] SongActivity.kt

--- app/src/main/java/com/narsha/wave_android/view/activity/song/SongActivity.kt
+++ app/src/main/java/com/narsha/wave_android/view/activity/song/SongActivity.kt
@@ -9,16 +9,20 @@ import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.YouTubePlayerListener
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.ui.views.YouTubePlayerSeekBar
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.ui.views.YouTubePlayerSeekBarListener
 
 
-class SongActivity : AppCompatActivity() {
+class SongActivity() : AppCompatActivity() {
     val arrayPlayDummy = listOf<String>("2COAu1l6hUQ", "jO2viLEW-1A", "HCc1UvTQ1Hg")
     var indexNowPlaying : Int = 0;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_song)
 
+        val youtubePlayerSeekBar : YouTubePlayerSeekBar = findViewById(R.id.youtube_player_seekbar)
         val youTubePlayerView: YouTubePlayerView = findViewById(R.id.youtube_player_view)
+
         lifecycle.addObserver(youTubePlayerView)
 
         val listener = object : YouTubePlayerListener {
@@ -44,9 +48,10 @@ class SongActivity : AppCompatActivity() {
                 if (state == PlayerConstants.PlayerState.ENDED) {
                     Log.d("DONE","ENDED")
                     indexNowPlaying ++;
-                    dataPlaying(youTubePlayer, indexNowPlaying)
+                    dataPlaying(youTubePlayer, indexNowPlaying, youtubePlayerSeekBar)
                 }else if(state == PlayerConstants.PlayerState.PLAYING){
                     Log.d("DONE","STARTING")
+                    youTubePlayer.addListener(youtubePlayerSeekBar)
                 }
             }
 
@@ -62,17 +67,19 @@ class SongActivity : AppCompatActivity() {
         }
         youTubePlayerView.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
             override fun onReady(youTubePlayer: YouTubePlayer) {
-                val videoId = arrayPlayDummy[indexNowPlaying]
-                youTubePlayer.loadVideo(videoId, 0F)
+                youTubePlayer.loadVideo("null", 0F) // 여기에 비디오 아이디 입력.
+                Log.d("?", youtubePlayerSeekBar.toString())
+                youTubePlayer.addListener(youtubePlayerSeekBar)
             }
         })
         youTubePlayerView.addYouTubePlayerListener(listener)
     }
 
-    fun dataPlaying(youTubePlayer : YouTubePlayer, index : Int) {
+    fun dataPlaying(youTubePlayer : YouTubePlayer, index : Int, youtubePlayerSeekBar : YouTubePlayerSeekBar) {
         if(index < arrayPlayDummy.size){
             val videoId = arrayPlayDummy[index]
             youTubePlayer.loadVideo(videoId, 0F)
+            youTubePlayer.addListener(youtubePlayerSeekBar)
         }
     }
 }
