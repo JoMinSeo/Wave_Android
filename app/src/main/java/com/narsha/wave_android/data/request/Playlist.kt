43e29b7e7d0ddf352615d37ede2c6f4573f4ed04
Jominseo <minseocho0309@gmail.com>
2020-10-28 16:46:35 +0900

    commit

---
+++ app/src/main/java/com/narsha/wave_android/data/request/Playlist.kt
@@ -0,0 +1,8 @@
+package com.narsha.wave_android.data.request
+
+data class Playlist(
+        var userId : String? = null,
+        var title : String? = null,
+        var mainGenre : String? = null,
+        var subGenre : String? = null
+)
