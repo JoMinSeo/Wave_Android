afd5f8ed7c90a27fa27df7dd28c01f28afc70d99
DGSW_양동현 <66651059+DGSWDongHyun@users.noreply.github.com>
2020-11-02 18:10:34 +0900

    [ADD] SearchedAdapter / ViewType .. etc.

---
+++ Wave_V1/app/src/main/java/com/narsha/wave_android/data/viewtype/SearchedViewType.kt
@@ -0,0 +1,11 @@
+package com.narsha.wave_android.data.viewtype
+
+class SearchedViewType {
+    object ViewType {
+        const val ARTIST = 0
+        const val MUSIC = 1
+        const val ALBUM = 2
+        const val MUSIC_BIG = 3
+        const val CATEGORY = 4
+    }
+}
