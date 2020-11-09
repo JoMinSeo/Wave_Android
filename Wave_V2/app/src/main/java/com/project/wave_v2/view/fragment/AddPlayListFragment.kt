18f06c99ecccd82b95b459f30dd214f5a6f0c029
kimjw2003 <kimjw111003@gmail.com>
2020-11-09 19:49:56 +0900

    [UPDATE] 내 플레이리스트 서버로 전송완료

--- Wave_V2/app/src/main/java/com/project/wave_v2/view/fragment/AddPlayListFragment.kt
+++ Wave_V2/app/src/main/java/com/project/wave_v2/view/fragment/AddPlayListFragment.kt
@@ -2,48 +2,52 @@ package com.project.wave_v2.view.fragment
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Context
 import android.content.DialogInterface
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.widget.*
 import androidx.fragment.app.DialogFragment
 import com.project.wave_v2.R
 import com.project.wave_v2.data.request.like.LikeFeelBody
+import com.project.wave_v2.data.request.playlist.CreatePlayListBody
+import com.project.wave_v2.data.response.ResultModel
 import com.project.wave_v2.data.response.like.LikeFeelModel
 import com.project.wave_v2.data.response.like.LikeGenreModel
 import com.project.wave_v2.network.RetrofitClient
-import kotlinx.android.synthetic.main.activity_genre.*
+import com.project.wave_v2.network.Service
+import kotlinx.android.synthetic.main.fragment_add_play_list.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class AddPlayListFragment : DialogFragment() {
 
-    var mainGenreList : List<LikeGenreModel>? = ArrayList<LikeGenreModel>()
-    var mainGenreNames : ArrayList<String> = ArrayList<String>()
-    var subGenreList : List<LikeFeelModel>? = ArrayList<LikeFeelModel>()
-    var subGenreNames : ArrayList<String> = ArrayList<String>()
+    var API: Service? = null
+
+
+    var mainGenreList: List<LikeGenreModel>? = ArrayList<LikeGenreModel>()
+    var mainGenreNames: ArrayList<String> = ArrayList<String>()
+    var subGenreList: List<LikeFeelModel>? = ArrayList<LikeFeelModel>()
+    var subGenreNames: ArrayList<String> = ArrayList<String>()
 
     lateinit var mainSpinner: Spinner
-    lateinit var subSpinner:Spinner
-    lateinit var mainAdapter : ArrayAdapter<*>
-    lateinit var subAdapter : ArrayAdapter<*>
+    lateinit var subSpinner: Spinner
+    lateinit var listName : EditText
+    lateinit var mainAdapter: ArrayAdapter<*>
+    lateinit var subAdapter: ArrayAdapter<*>
 
     lateinit var mainRequest: Call<List<LikeGenreModel>>
-    lateinit var subRequest:Call<List<LikeFeelModel>>
+    lateinit var subRequest: Call<List<LikeFeelModel>>
 
-    lateinit var dialogView:View
-    var mainGenreId=0
-    var subGenreId=0
+    lateinit var dialogView: View
+    lateinit var userId: String
 
-    var mainSpinnerListener= object : AdapterView.OnItemSelectedListener {
+    var mainGenreId = 0
+    var subGenreId = 0
+
+    var mainSpinnerListener = object : AdapterView.OnItemSelectedListener {
         override fun onItemSelected(
             parent: AdapterView<*>?,
             view: View?,
@@ -53,10 +57,11 @@ class AddPlayListFragment : DialogFragment() {
             mainGenreId = position + 1
             callSubGenre(mainGenreId)
         }
+
         override fun onNothingSelected(parent: AdapterView<*>?) {}
     }
 
-    var subSpinnerListener=object : AdapterView.OnItemSelectedListener {
+    var subSpinnerListener = object : AdapterView.OnItemSelectedListener {
         override fun onItemSelected(
             parent: AdapterView<*>?,
             view: View?,
@@ -65,22 +70,23 @@ class AddPlayListFragment : DialogFragment() {
         ) {
             subGenreId = position + 1
         }
+
         override fun onNothingSelected(parent: AdapterView<*>?) {}
     }
 
-    private fun callMainGenre(){
+    private fun callMainGenre() {
         mainRequest = RetrofitClient.getService()?.likeGenre()!!
-        mainRequest.enqueue(object : Callback<List<LikeGenreModel>>{
+        mainRequest.enqueue(object : Callback<List<LikeGenreModel>> {
             override fun onResponse(
                 call: Call<List<LikeGenreModel>>,
                 response: Response<List<LikeGenreModel>>
             ) {
-                if(response.code()==200){
+                if (response.code() == 200) {
                     mainGenreList = response.body()
-                    mainGenreList?.let{
+                    mainGenreList?.let {
                         mainGenreNames.clear()
                         val iterator = it.iterator()
-                        while(iterator.hasNext()){
+                        while (iterator.hasNext()) {
                             iterator.next().mainGenreName?.let { mainGenreNames.add(it) }
                         }
                         mainAdapter.notifyDataSetChanged()
@@ -93,20 +99,21 @@ class AddPlayListFragment : DialogFragment() {
             }
         })
     }
-    private fun callSubGenre(mainGenreId:Int){
-        val likeFeelBody=LikeFeelBody(mainGenreId)
+
+    private fun callSubGenre(mainGenreId: Int) {
+        val likeFeelBody = LikeFeelBody(mainGenreId)
         subRequest = RetrofitClient.getService()?.likeFeel(likeFeelBody)!!
-        subRequest.enqueue(object : Callback<List<LikeFeelModel>>{
+        subRequest.enqueue(object : Callback<List<LikeFeelModel>> {
             override fun onResponse(
                 call: Call<List<LikeFeelModel>>,
                 response: Response<List<LikeFeelModel>>
             ) {
-                if(response.code()==200){
+                if (response.code() == 200) {
                     subGenreList = response.body()
-                    subGenreList?.let{
+                    subGenreList?.let {
                         subGenreNames.clear()
                         val iterator = it.iterator()
-                        while(iterator.hasNext()){
+                        while (iterator.hasNext()) {
                             iterator.next().subGenreName?.let { subGenreNames.add(it) }
                         }
                         subAdapter.notifyDataSetChanged()
@@ -121,6 +128,9 @@ class AddPlayListFragment : DialogFragment() {
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        userId = requireActivity().getSharedPreferences("user_info", Context.MODE_PRIVATE)
+            .getString("userId", "").toString()
+
         initWidgets()
         var listener = DialogListener()
 
@@ -133,12 +143,14 @@ class AddPlayListFragment : DialogFragment() {
         return alert
     }
 
-    private fun initWidgets(){
+    private fun initWidgets() {
         dialogView = layoutInflater.inflate(R.layout.fragment_add_play_list, null)
         mainSpinner = dialogView.findViewById(R.id.mainGenre_Sp)
         subSpinner = dialogView.findViewById(R.id.subGenre_Sp)
         mainSpinner.onItemSelectedListener = mainSpinnerListener
         subSpinner.onItemSelectedListener = subSpinnerListener
+
+        listName = dialogView.findViewById(R.id.playlistTitle_Et)
 
         mainAdapter = ArrayAdapter(
             requireContext(),
@@ -159,11 +171,33 @@ class AddPlayListFragment : DialogFragment() {
         callMainGenre()
     }
 
+    private fun mainInfoSetting(listName : String) {
+
+        API = RetrofitClient.getService()
+        val data = CreatePlayListBody(userId, listName, mainGenreId.toString(), subGenreId.toString())
+        API?.createPlayList(data)
+            ?.enqueue(object : Callback<ResultModel> {
+                override fun onResponse(call: Call<ResultModel>, response: Response<ResultModel>) {
+                    if (response.code() == 200) {
+                        dismiss()
+                    }
+                }
+                override fun onFailure(call: Call<ResultModel>, t: Throwable) {
+
+                }
+            })
+        }
+
     inner class DialogListener : DialogInterface.OnClickListener{
         override fun onClick(dialog: DialogInterface?, which: Int) {
             when(which) {
                 DialogInterface.BUTTON_POSITIVE -> { // 확인 버튼
-
+                    if (listName.text.isEmpty()) {
+                        Toast.makeText(requireContext(), "플레이리스트의 이름을 입력해주세요", Toast.LENGTH_SHORT).show()
+                    } else {
+                        mainInfoSetting(listName.text.toString())
+                    }
+                    Log.d("Logd", "확인버튼입력됨")
                 }
                 DialogInterface.BUTTON_NEGATIVE -> { // 취소 버튼
                 }
